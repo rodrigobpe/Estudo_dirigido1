0e90da440f0c406f7fe7cb60e7a79c66182f4818
marlon-azevedo <48135703+marlon-azevedo@users.noreply.github.com>
2019-03-01 16:22:45 -0300

    Add files via upload

---
+++ exercício1.php
@@ -0,0 +1,8 @@
+<?php
+
+    print "Valor em metros: ";
+    $valor_em_metros = (float) fgets (STDIN);
+
+    $valor_em_milimetros = $valor_em_metros*1000;
+
+    print "Seu valor em milímetros é igual a: $valor_em_milimetros \n";
